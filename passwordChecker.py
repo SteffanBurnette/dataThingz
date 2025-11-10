0ec30ec9513cc1e4f96caa41663c6f2754d96af2
SteffanBurnette <burnettesteffanep7@gmail.com>
2025-11-10 09:16:46 -0500

    Virtual Environment

---
+++ passwordChecker.py
@@ -0,0 +1,14 @@
+def is_strong_password(password):
+    """"This function checks if the password is strong or not"""
+    if len(password) <8:
+        return False
+    if not any(char.isdigit() for char in password):
+        return False
+    if not any(char.islower() for char in password):
+        return False
+    if not any(char.isupper() for char in password):
+        return False
+    if not any(char in '!@#$%^&*()_+?' for char in password):
+        return False
+        
+    return True
