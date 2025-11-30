2616f8741146c1c4e2886fa58e0e4b398db2388f
SteffanBurnette <burnettesteffansep7@gmail.com>
2025-11-29 20:03:54 -0500

    h

--- python==3.14.0/pandTest.py
+++ python==3.14.0/pandTest.py
@@ -3,3 +3,14 @@ import pandas as pd
 df = pd.read_csv("C:/Users/Steffan/Desktop/Ude-Science-Data/dataThingz/python==3.14.0/nyc_SAT_scores.csv")
 
 print(df.head()) 
+
+
+
+dfn = pd.DataFrame({
+    'category': ['A', 'B', 'A', 'C', 'B', 'A'],
+    'value': [10, 20, 15, 25, 30, 12]
+})
+
+grouped_df = dfn.groupby('category')["value"].mean()
+
+print(grouped_df.head())
